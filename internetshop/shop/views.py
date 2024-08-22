d5ce0d5df9d4aea7eb4ec956dea20bba45037013
Zalman <bc.wjs@gmail.com>
2024-08-22 20:10:03 +0400

    добавила товары

--- internetshop/shop/views.py
+++ internetshop/shop/views.py
@@ -2,5 +2,9 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
 def home(request):
-    return HttpResponse("Hello")
+    return render(request,"index.html")
+
+
+
